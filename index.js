1307dc0874aa4415643aa44213da9705fdb996c4
Jasec <jasec_7@hotmail.com>
2025-01-05 02:57:34 -0800

    complete objects lab

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Write your solution in this file!
+const employee = { 
+    name: "Jasec",
+    streetAddress: "229 Clement"
+};
+
+function updateEmployeeWithKeyAndValue(obj,key,value) {
+    return {
+        ...obj,
+    [key]: value,
+ };
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(obj, key, value) {
+    obj[key] = value;
+    return obj;
+}
+employee.streetAddress = "12 Broadway"
+
+function deleteFromEmployeeByKey(obj, key) {
+    return {
+        ...obj[key],
+    }
+};
+delete employee.name;
+
+
+
+
+function destructivelyDeleteFromEmployeeByKey(obj, key) {
+    delete obj[key]
+    return obj;
+}
+
+
